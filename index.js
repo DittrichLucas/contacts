8137db1bb51309ccababf5be888b828bf7261f0b
Lucas Oliveira Dittrich <lucas.oliveira@ng.local>
2018-09-12 13:27:02 -0300

    Regra de negócio.

--- index.js
+++ index.js
@@ -1,29 +1,80 @@
 var express = require('express');
-var test = express();
+var bodyParser = require('body-parser');
 
+var server = express();
+
+let proximoId = 1;
 let agenda = [];
 
+server.use(bodyParser.json());
+
 // Buscar todos os contatos
-test.get('/contatos', (req, res) => {
+server.get('/contatos', (req, res) => {
+	res.send(agenda)
 });
 
 // Buscar um contato pelo id
-test.get('/contatos/:id', (req, res) => {
+server.get('/contatos/:id', (req, res) => {
+
+	for(let i = 0; i < agenda.length; i++){
+		if(Number(req.params.id) === agenda[i].id){
+			res.send(agenda[i])
+			return;
+		}
+	}
+
+	res.send({message: 'Contato não encontrado!'})
 });
 
 // Criar um contato
-test.post('/contatos', (req, res) => {
+server.post('/contatos', (req, res) => {
+	let contato = req.body
+
+	if (contato.nome === undefined || contato.email === undefined) {
+		res.send({ message: 'Está faltando o nome ou o email' })
+	} else {
+		contato.id = proximoId++;
+		agenda.push(contato)
+		res.send({ message: 'Usuário criado!' })
+	}
 });
 
 // Alterar um contato pelo id
-test.put('/contatos/:id', (req, res) => {
+server.put('/contatos/:id', (req, res) => {
+	let contato = req.body
+
+	if (contato.nome === undefined || contato.email === undefined) {
+		res.send({ message: 'Está faltando o nome ou o email' })
+
+	} else {
+
+		for(let i = 0; i < agenda.length; i++){
+			if(Number(req.params.id) === agenda[i].id){
+				agenda[i].nome = contato.nome
+				agenda[i].email = contato.email
+
+				res.send({message: 'Usuário alterado!'})
+				return;
+			}
+		}
+		res.send({message: 'Contato não encontrado!'})
+	}
 });
 
 // Deletar um contato pelo id
-test.delete('/contatos/:id', (req, res) => {
+server.delete('/contatos/:id', (req, res) => {
+	const id = Number(req.params.id)
+
+	agendaFiltrada = agenda.filter(contato => contato.id !== id)
+
+	if (agendaFiltrada.length === agenda.length) {
+		res.send({message: 'Contato não encontrado!'})
+	} else {
+		res.send({message: 'Usuário deletado!'})
+	}
 });
 
-test.listen(3000, () => {
+server.listen(3000, () => {
 	console.log('Example app listening on port 3000!');
 });
 
