59c87de2e05c2e1da3d443059b20ddef6347f78d
Lucas Dittrich <ld.dittrich@gmail.com>
2020-09-22 18:07:51 -0300

    [refactor] rename database tables, fields and messages
    Signed-off-by: Lucas Dittrich <ld.dittrich@gmail.com>

--- src/services/contact.ts
+++ src/services/contact.ts
@@ -5,13 +5,13 @@ export async function create(
     email: string,
     userId: number
     ) {
-        const text = `INSERT INTO schedule (name, email, user_id)
+        const text = `INSERT INTO contacts (name, email, user_id)
             VALUES ($1, $2, $3) RETURNING *`
         const value = [name, email, userId]
 
         await db.query(text, value)
 
-    return { message: 'User created!' }
+    return { message: 'Contact created!' }
 }
 
 export async function update(
@@ -20,18 +20,18 @@ export async function update(
     id: number,
     userId: number
 ) {
-    const text = `UPDATE schedule
+    const text = `UPDATE contacts
         SET name = $1, email = $2
         WHERE id = $3 AND user_id = $4;`
     const value = [name, email, id, userId]
 
     await db.query(text, value)
 
-    return { message: 'User updated!' }
+    return { message: 'Contact updated!' }
 }
 
 export async function remove(id: number, userId: number) {
-    const text = `DELETE FROM schedule
+    const text = `DELETE FROM contacts
         WHERE id = $1 AND user_id = $2 RETURNING *`
     const value = [id, userId]
     const res = await db.query(text, value)
@@ -40,11 +40,11 @@ export async function remove(id: number, userId: number) {
         return { message: 'Contact not found!' }
     }
 
-    return { message: 'User removed!' }
+    return { message: 'Contact removed!' }
 }
 
 export async function findByUserId(userId: number) {
-    const text = 'SELECT * FROM schedule WHERE user_id = $1;'
+    const text = 'SELECT * FROM contacts WHERE user_id = $1;'
     const value = [userId]
     const res = await db.query(text, value)
 
@@ -52,7 +52,7 @@ export async function findByUserId(userId: number) {
 }
 
 export async function findById(id: number, userId: number) {
-    const text = 'SELECT * FROM schedule WHERE id = $1 user_id = $2;'
+    const text = 'SELECT * FROM contacts WHERE id = $1 user_id = $2;'
     const value = [id, userId]
     const res = await db.query(text, value)
 
