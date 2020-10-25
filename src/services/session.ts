0e8438135d5749fd1e472a89fa4a083c62cf3495
Lucas Dittrich <ld.dittrich@gmail.com>
2020-10-25 00:29:28 -0300

    [feat] transform entity session in a class
    Signed-off-by: Lucas Dittrich <ld.dittrich@gmail.com>

--- src/services/session.ts
+++ src/services/session.ts
@@ -2,31 +2,35 @@ import * as crypto from 'crypto'
 
 import { pool as db } from '../db'
 
-export async function create(email: string, password: string) {
-    const query = 'SELECT * FROM users WHERE email = $1'
-    const resUser = await db.query(query, [email])
-    const user = resUser.rows[0]
+export default class SessionService {
 
-    if (user && user.password === password) {
-        const query = 'INSERT INTO session (token, user_id) VALUES ($1, $2)'
-        const token = crypto.randomBytes(32).toString('hex')
-        await db.query(query, [token, user.id])
+    async login(email: string, password: string) {
+        const query = 'SELECT * FROM users WHERE email = $1'
+        const resUser = await db.query(query, [email])
+        const user = resUser.rows[0]
 
-        return token
-    } else {
-        throw new Error('Incorrect username or password!')
+        if (user && user.password === password) {
+            const query = 'INSERT INTO session (token, user_id) VALUES ($1, $2)'
+            const token = crypto.randomBytes(32).toString('hex')
+            await db.query(query, [token, user.id])
+
+            return token
+        } else {
+            throw new Error('Incorrect username or password!')
+        }
+    }
+
+    async logout(token: string, userId: number) {
+        const text = `DELETE FROM session
+            WHERE token = $1 AND user_id = $2 RETURNING *`
+        const value = [token, userId]
+        const res = await db.query(text, value)
+
+        if (res.rows.length === 0) {
+            return { message: 'Contact not found!' }
+        }
+
+        return { message: 'Closed session!' }
     }
 }
 
-export async function remove(token: string, userId: number) {
-    const text = `DELETE FROM session
-        WHERE token = $1 AND user_id = $2 RETURNING *`
-    const value = [token, userId]
-    const res = await db.query(text, value)
-
-    if (res.rows.length === 0) {
-        return { message: 'Contact not found!' }
-    }
-
-    return { message: 'Closed session!' }
-}
