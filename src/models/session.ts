a650a4a4bd6a63304e58e09d2227521088718a3a
lucas.dittrich <ld.dittrich@gmail.com>
2020-11-05 21:46:47 -0300

    [feat] add session model
    Signed-off-by: lucas.dittrich <ld.dittrich@gmail.com>

---
+++ src/models/session.ts
@@ -0,0 +1,11 @@
+import { Entity, OneToOne, PrimaryColumn } from "typeorm";
+import { User } from "./user";
+
+@Entity()
+export class Session {
+    @PrimaryColumn()
+    token: string
+
+    @OneToOne(() => User, user => user.id)
+    userId: User
+}
